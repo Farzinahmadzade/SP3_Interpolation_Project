9b3eb8033a2da420ae0980ca74ec8915f3a2ddc5
Farzin <farzinahmadzade909@gmail.com>
2025-12-02 10:55:27 +0330

    Update plot_sp3_only.py

--- plot_sp3_only.py
+++ plot_sp3_only.py
@@ -7,7 +7,7 @@ df = pd.read_csv("Data/g05_sp3_interp_30s.csv")
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
-ax.plot(df["X"], df["Y"], df["Z"], color="red", linewidth=1.0)
+ax.plot(df["X_m"], df["Y_m"], df["Z_m"], color="red", linewidth=1.0)
 
 ax.set_xlabel("X (m)")
 ax.set_ylabel("Y (m)")
