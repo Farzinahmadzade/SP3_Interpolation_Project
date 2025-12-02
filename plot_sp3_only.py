172e3da06761be6a21f22f2ca1ecf3959e71c672
Farzin <farzinahmadzade909@gmail.com>
2025-12-02 10:33:34 +0330

    Create plot_sp3_only.py

---
+++ plot_sp3_only.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # noqa
+
+df = pd.read_csv("Data/g05_sp3_interp_30s.csv")
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+ax.plot(df["X"], df["Y"], df["Z"], color="red", linewidth=1.0)
+
+ax.set_xlabel("X (m)")
+ax.set_ylabel("Y (m)")
+ax.set_zlabel("Z (m)")
+ax.set_title("3D Orbit of G05 from interpolated SP3 (30 s)")
+
+plt.tight_layout()
+plt.savefig("G05_SP3_orbit_3d.png", dpi=300)
+plt.show()
