17a51fb4415357918bdd14bdc301fa9e4f9b4820
Farzin <farzinahmadzade909@gmail.com>
2025-12-03 11:37:12 +0330

    Update get_time_range_sp3.py

--- get_time_range_sp3.py
+++ get_time_range_sp3.py
@@ -1,22 +1,15 @@
 """
-get_time_range_sp3.py - Extract time range from SP3 CSV (no PRN needed)
+Module Name : get_time_range_sp3.py
+Description : Read SP3 data for a given PRN and extract the first and last valid epochs for that satellite.
 
-Author: F. Ahmadzade
+Author      : F.Ahmadzade
 """
 
 import pandas as pd
 from typing import Tuple
 
 def get_time_range(sp3_csv_path: str) -> Tuple[pd.Timestamp, pd.Timestamp]:
-    """
-    Extract start and end gps_time from SP3 CSV file
 
-    Args:
-        sp3_csv_path: Path to CSV (e.g., 'g05_sp3_raw.csv')
-
-    Returns:
-        Tuple[pd.Timestamp, pd.Timestamp]: (start_time, end_time)
-    """
     df = pd.read_csv(sp3_csv_path, parse_dates=['gps_time'])
     
     if df.empty:
@@ -26,7 +19,6 @@ def get_time_range(sp3_csv_path: str) -> Tuple[pd.Timestamp, pd.Timestamp]:
     end_time = df['gps_time'].max()
     return start_time, end_time
 
-# Test code
 if __name__ == "__main__":
     file_path = r"K:\GitHub\sp3_interpolation_project\g05_sp3_raw.csv"
     
